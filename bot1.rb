c25f8ad49833c589cc1a4ca79bcebc00e4b4fcd8
christopheleray <christophe.leray1@gmail.com>
2018-07-11 12:10:42 +0200

    Mes premiers bot twitter en ruby

---
+++ bot1.rb
@@ -0,0 +1,19 @@
+ # ligne très importante qui appelle la gem. Sans elle, le programme ne saura pas appeler Twitter
+require 'twitter'
+require 'dotenv'
+require 'pry'
+
+Dotenv.load
+
+# quelques lignes qui enregistrent les clés d'APIs
+client = Twitter::REST::Client.new do |config|
+  config.consumer_key        = ENV["TWITTER_API_KEY"]
+  config.consumer_secret     = ENV["TWITTER_API_SECRET"]
+  config.access_token        = ENV["TWITTER_TOKEN"]
+  config.access_token_secret = ENV["TWITTER_TOKEN_SECRET"]
+
+end
+#binding.pry
+p client
+# ligne qui permet de tweeter
+client.update('Mon premier tweet en ruby avec @the_hacking_pro!!!!')
